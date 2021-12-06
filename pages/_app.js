15d59e9b50ee145cee99111260532c0503e0f109
Martin Chammah <martin2844@gmail.com>
2021-12-06 20:41:31 -0300

    norobots

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import { CssBaseline } from "@nextui-org/react";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta key="robots" name="robots" content="noindex,follow" />
+        <meta key="googlebot" name="googlebot" content="noindex,follow" />
+      </Head>
       <CssBaseline />
       <Component {...pageProps} />
     </>
